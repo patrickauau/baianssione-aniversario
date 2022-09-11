dc3f824d84c38ebe6701399cfed1af205bb17597
patrickauau <99296414+patrickauau@users.noreply.github.com>
2022-09-10 22:11:11 -0300

    arrumei o erro

---
+++ app.js
@@ -0,0 +1,167 @@
+function descricao(membro){
+
+    let audio = document.getElementById("audio")
+
+    if(membro==="patrick"){
+
+        alertify.alert("Patrick","Fundador da Mafia Brasileira Baiasones. Maior Inimigo das Woman Faces e segue o doutor fran em todas as redes sociais",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="joao"){
+
+        alertify.alert("João","Sua natureza é desconhecida nos anos atuais por ser um dos Sigmas restantes nesta terra. Palavras favoritas: var e codepen.io. Hobbies: Roubo internacional de IP's", function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+
+        })
+
+    }else if(membro==="rodrigo"){
+
+        alertify.alert("Rodrigo","Violencia sempre resolve :)",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="lipe"){
+
+        alertify.alert("Lipe","bom dia, o de cima e gay fuck owofuck owofuck owofuck")
+
+    }else if(membro==="extremo"){
+
+        alertify.alert("Extremo","um dos mais ativos no chat do grupo do goku",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="pye"){
+
+        alertify.alert("Pye","pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo pye martelo",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="blacitu"){
+
+        alertify.alert("Blacitu","não tem 500gb de child porn mas tem 500gb de print random",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="might"){
+
+        alertify.alert("Might","ping diario no jose perguntando se ele já upou no jogo",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="azy"){
+
+        alertify.alert("Azy","black ate hoje tem duvida se é homem ou mulher",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="stylo"){
+
+        alertify.alert("Stylo","no haki????? :((((((((((((((((( (tem chance alta de spawnar na call entre os horarios 21:00-23:00 para comentar sobre tristeza)",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="arthur"){
+
+        alertify.alert("Arthur","olha meu lancha chamas que pica",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="jose"){
+
+        alertify.alert("Jose","sempre dizendo que ta com preguiça de upar pro might",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="edu"){
+
+        alertify.alert("Edu","kakkakakakkakakakakakkakakakkakakkaakkakakkakakakakakakakakaka do que voces tao falando kakakakakkakakkakaka",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+            
+    }else if(membro==="gabriel"){
+
+        alertify.alert("Gabriel","é mais facil o cego nos ver do que nos ver ele aparecer ",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="macakinos"){
+
+        alertify.alert("Macakinos","nao sei nao convivo direto mas muito pica",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="marcelo"){
+
+        alertify.alert("Marcelo","sermaloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo sempre cita a mae do stylo",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="player"){
+
+        alertify.alert("Player","gabriel 2 so que aparece pra postar meme aleatorio e pra dizer que o pc queimou e a sempre adiando a data",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="ravi"){
+
+        alertify.alert("Ravi","ezezezezezezezezezezezezez anda com um portugues que sempre descobre merda no discord muito foda",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro==="torresmo"){
+
+        alertify.alert("Torresmo","acompanha lore de fortnite por inteira mas nao aacompanha a historia de vida do stylo",function(){
+
+            alertify.message("FELIZ ANIVERSARIO BAIANSSIONE @everyone")
+
+        })
+
+    }else if(membro=="groovy"){
+
+        alertify.alert("Groovy", "Bravo Guerreiro e DJ que batalhou contra Alok e Player Tauz ao mesmo tempo, porém, foi derrotado e desativado", function(){
+
+            alertify.message("FELIZ ANIVERSÁRIO BAIASSIONE @everyone")
+
+        })
+
+        audio.currentTime = 15
+        audio.play()
+
+    }
+
+}
